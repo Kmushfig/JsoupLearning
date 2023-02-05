ec6b207c3f056704c0592ae5de5d1457df9854ff
Professional <kazimovmu@gmail.com>
2023-02-06 00:02:40 +0400

    Initial commit

---
+++ src/main/java/com/jsouplearning/jsouplearning/JsoupLearningApplication.java
@@ -0,0 +1,13 @@
+package com.jsouplearning.jsouplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoupLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsoupLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsouplearning/jsouplearning/JsoupLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsouplearning.jsouplearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsoupLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
