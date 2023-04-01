02f029ed3fd92d5784a432aa372e3f23796c06cb
Professional <kazimovmu@gmail.com>
2023-04-02 01:01:39 +0400

    Message last versionnn

--- src/main/java/com/jsouplearning/jsouplearning/ModelsOfTheCars.java
+++ src/main/java/com/jsouplearning/jsouplearning/ModelsOfTheCars.java
@@ -11,19 +11,31 @@ import org.jsoup.select.Elements;
 public class ModelsOfTheCars {
     public static void main(String[] args) throws IOException {
 
-        HashMap<String, String> modelIds = new HashMap<>();
+//        HashMap<String, String> modelIds = new HashMap<>();
 
         Document doc = Jsoup.connect("https://turbo.az/").get();
 
-
+        Elements end = doc.getElementsByClass("main-search__row main-search__row-4 tz-d-grid");
 //        Elements options = doc.getElementsByClass("js-search-select-make").select("option");
-        Elements options2 = doc.getElementsByClass("js-search-select-model").select("option");
+//        Elements options2 = doc.getElementsByClass("js-search-select-model").select("option");
 
-        for (Element carModel : options2 ) {
-            String modelName = carModel.text();
-            String modelId = carModel.val();
-            String makeId = carModel.attr("class");
-            System.out.println(makeId + "  " + modelName + " " + modelId + "\n");
+        for (Element carModel : end) {
+
+            String makeName = carModel.getElementsByClass("js-search-select-make").text();
+            String makeId = carModel.getElementsByClass("js-search-select-model").attr("class");
+            String modelName = carModel.getElementsByClass("js-search-select-model").text();
+            String modelId = carModel.getElementsByClass("js-search-select-model").val();
+//            Elements elements = doc.getElementsByClass("select optional form-control js-search-select-make");
+//            String makeId = elements.attr("class");
+//            String makeName = carModel.text();
+//            Elements modelElements = doc.getElementsByClass("js-search-select-model");
+//            String modelName = modelElements.text();
+//            String modelId = modelElements.val();
+
+            System.out.println(makeName);
+            System.out.println(makeId);
+            System.out.println(modelName);
+            System.out.println(modelId);
         }
     }
 }
